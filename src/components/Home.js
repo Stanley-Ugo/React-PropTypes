6859f63cfd5b5511e4e7c768535f16a49d26b08d
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-08 11:40:55 +0100

    adding state to the Home component and passing it as props to <Profile />

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,21 @@ import Profile from './Profile';
 
 class HomeComponent extends Component{
 
-    
+    state = {
+        name:'Francis',
+        lastname:'Jones',
+        age:23,
+        hobbies:['running','jumping'],
+        spanish:false,
+        message(){console.log('hello')},
+        car:{brand:'Ford',model:"Mustang"},
+        mother:'Martha'
+    }
 
     render(){
         return(
             <>
-                <Profile />
+                <Profile {...this.state}/>
             </>
 
         )
